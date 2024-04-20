4f6a4523702734275cf9fc1c99b956abc2f70ef7
AykhanMikayilzada <ayxan.mikayilzadee@gmail.com>
2024-04-20 04:12:14 +0400

    problem-fixing

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,15 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-export const auth = firebase.initializeApp({
-    apiKey: "AIzaSyCq9cnPgAdgZ2YFnpqWNdcO_9Sl6OAsMiw",
-    authDomain: "oniochat.firebaseapp.com",
-    projectId: "oniochat",
-    storageBucket: "oniochat.appspot.com",
-    messagingSenderId: "539573199124",
-    appId: "1:539573199124:web:58ca28aec1199ca8c36201"
-  }).auth();
+const firebaseConfig = {
+  apiKey: "AIzaSyCq9cnPgAdgZ2YFnpqWNdcO_9Sl6OAsMiw",
+  authDomain: "oniochat.firebaseapp.com",
+  projectId: "oniochat",
+  storageBucket: "oniochat.appspot.com",
+  messagingSenderId: "539573199124",
+  appId: "1:539573199124:web:58ca28aec1199ca8c36201"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+export const auth = app.auth();
